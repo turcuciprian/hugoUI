595535ae33047c376e469f141e6d4cd83a1f9fb0
Ciprian Turcu <turcuciprian1@gmail.com>
2017-09-05 15:25:54 +0300

    initial commit

---
+++ hugoUI.php
@@ -0,0 +1,11 @@
+<?php
+/*
+  Plugin Name: hugoUI
+  Plugin URI: http://ciprianturcu.com
+  Description: plugin that turns wordpress into a machine of static content generating using hugo.
+  Version: 0.1
+  Author: turcuciprian
+  Author URI: http://ciprianturcu.com
+  License: GPLv2 or later
+  Text Domain: rta
+ */
